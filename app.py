aef448c66f7cb2a5c721d1595d74d94a6c3e78ca
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-06-08 12:03:49 +0600

    Added HTML template

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask import render_template
+
 import sys
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('index.html')
+
 # don't change the following way to run flask:
 if __name__ == '__main__':
     if len(sys.argv) > 1:
